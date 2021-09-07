75a69cf5dfade0d1bc46552f0fedb80e660de878
burakizm <88186150+burakizm@users.noreply.github.com>
2021-09-07 08:55:30 +0300

    Create start.py

---
+++ start.py
@@ -0,0 +1,27 @@
+from pyrogram import Client, filters
+
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup 
+
+@Client.on_message(filters.command("start"))
+
+async def start(client, m: Message):
+
+   if m.chat.type == 'private':
+
+       await m.reply(f"**Merhaba!Ben telegram sesli sohbetlerinde videoları görüntülü bir şekilde yayınlayabilen botum. \n\n**yardım için /help To komutlar için:-** __ \n1) Type /info kurucu`",   
+
+                            reply_markup=InlineKeyboardMarkup(
+
+                                [[
+
+                                     InlineKeyboardButton(
+
+                                            "Dev", url="https://t.me/burakizm")
+
+                                    ]]
+
+                            ))
+
+   else:
+
+      await m.reply("****")
